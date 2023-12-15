4fe1c2e14dae2041963f794f00b9987ba7307bed
joaopamarall <joaoamaral56@hotmail.com>
2023-12-15 12:53:46 -0300

    refactor: Refatorar código para melhor leitura de lógica.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import './App.css'
 
-function App() {
-  function handleClick() {
-    alert('You clicked me!');
-  }
+function HandleClick() {
+  const alertBtn = () => {
+    window.alert('Você clicou no botão!');
+  };
+  
+  return (
+    <>
+      <button onClick={alertBtn}>
+        Clique-me!
+      </button>
+    </>
+  )
+}
 
+function App() {
+  
   return (
     <>
       <h1>Teste para vaga de estágio em React!</h1>
+      <h3>Questão 1</h3>
       <div className="card">
-      <button onClick={handleClick}>
-        Clique-me!
-      </button>
+        <HandleClick />
       </div>
     </>
   )
